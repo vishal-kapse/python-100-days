004018a83c1234004952eea9401c199b2e6e04cf
vishal-kapse <vishal.kapse@cldcvr.com>
2022-07-02 17:51:52 +0530

    day1 submission

---
+++ day-1-band-name-generator/main.py
@@ -0,0 +1,5 @@
+print("Welcome to the band generator. \n Please provide below inputs... \n")
+
+city = input("Which city you were born in? \n")
+pet = input("What is the name of your pet? \n")
+print(f"The possible band name that you can try is {city} {pet}")
